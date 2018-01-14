7a0f5c2006884e413123474eb218d63867918c62
alainduchateau <alain.duchateau@gmail.com>
2018-01-14 20:22:51 +0100

    base

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', ['uses' => 'WelcomeController@index', 'as' => 'home']);
+
+Route::get('/article/{n}','ArticleController@show')->where('n','[0-9]+');
+
+Route::get('users', 'UsersController@getInfos');
+
+Route::post('users', 'UsersController@postInfos');
+
+Route::get('contact', 'ContactController@getForm');
+
+Route::post('contact', 'ContactController@postForm');
